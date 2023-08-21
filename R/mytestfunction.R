26882878bbdbc74aa4d55fdf48fabf6c26696072
立本博文 <hirofumi.tatsumoto@gmail.com>
2023-08-21 10:38:11 +0900

    first commit

---
+++ R/mytestfunction.R
@@ -0,0 +1,15 @@
+#自分のRオブジェクトなどを定義する
+printHello = function(){ print("Hello R world")}
+
+myvar1 = c(1:100)
+myvar2 = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+myvar3 = c("Jan", "Feb", "Mar", "Apr", "May",	"Jun", "Jul",	"Aug", "Sep", "Oct", "Nov",	"Dec")
+
+
+
+myfoo1_func = function(){print("myfoo1_func")}
+
+myfoo2_func = function(){print("myfoo2_func")}
+
+
+
